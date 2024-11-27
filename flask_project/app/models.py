cc00cc963d35e55b6797b6ed44755dbe1e57cde9
haylie-pitt <htc8@pitt.edu>
2024-11-27 11:35:24 -0500

    Log in page, User authentication, Search Bar Functionality, Settings Page

--- flask_project/app/models.py
+++ flask_project/app/models.py
@@ -1,13 +1,23 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 from . import db
 
-class Account(db.Model):
+class Account(db.Model, UserMixin):  # Add UserMixin here
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(100), nullable=False)
     desc = db.Column(db.String(255))
     hobbies = db.Column(db.String(255))
     age = db.Column(db.String(20))
     event_attendance = db.relationship('Event', secondary='event_attendance')
+
+    # Set the password after hashing
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    # Check if the password matches the hashed password
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
